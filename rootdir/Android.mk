ca18a42c52e8b0207e64cff0696282b8a6b06762
hirero-kazuwa <hirero00@gmail.com>
2024-04-10 17:28:13 +0530

    sm8550-common: update modules
    Signed-off-by: hirero-kazuwa <hirero00@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -103,3 +103,28 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd-odm.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM_ETC)
 include $(BUILD_PREBUILT)
+
+# Recovery init configuration files
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.hardware.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.hardware.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := miui.factoryreset.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/miui.factoryreset.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
